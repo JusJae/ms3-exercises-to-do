177d40fb1e53a97529cbe9038a6f90ed5e6d3287
Jus Jae <42322548+JusJae@users.noreply.github.com>
2023-10-07 19:19:58 +0100

    (render): flask base template

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 import os
-from flask import Flask
+from flask import (Flask, flash, render_template, 
+                url_for,request, redirect, session)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
-# Set up the app configuration
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+def base():
+    return render_template("base.html")
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")), #type: ignore
+            port=int(os.environ.get("PORT")),
             debug=True)
